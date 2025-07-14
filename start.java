14d5ce0b9cfe924e5dcbe4a9c83e2ac557f774af
Prince <138170521+heyprincehere@users.noreply.github.com>
2025-07-14 10:58:26 +0530

    Add start.java

---
+++ start.java
@@ -0,0 +1,18 @@
+class start{
+    public static void main(String[] args) {
+        //integer
+        int a=10;
+        //float with type casting
+        float ab = (float) 10.7;
+        //print
+        System.out.println(a);
+        //string
+        System.out.print("Today is monday");
+        
+    
+    
+    
+    
+    
+    }
+}
